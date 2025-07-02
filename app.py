c731a969af2e3f06bdec907d35dd0b636a058e55
LiuYijie <1148781323@qq.com>
2025-07-02 20:29:31 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
-from flask import Flask, url_for
+from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
+from flask import Flask
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://lyj:20030626@localhost/flaskweb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return "index page"
 
 @app.route('/user/<name>')
 def user_page(name):
     return f'User: {escape(name)}'
 
-@app.route('/test')
-def test_url_for():
-    print(url_for('index'))
-    print(url_for('user_page', name='greyli'))
-    print(url_for('user_page', name='peter'))
-    print(url_for('test_url_for'))
-    print(url_for('test_url_for', num=2))
-    return 'Test page'
+@app.route('/operation/add/<int:x>/<int:y>')
+def add(x, y):
+    return f'{x} + {y} = {x + y}'
 
 if __name__ == '__main__':
     app.run()
